54cea97027e938897439e968bd2503cd2fcf7ca9
Nikita <nikis2345@mail.ru>
2022-08-25 15:35:52 +0300

    dz

--- hello_django/hello_django/settings.py
+++ hello_django/hello_django/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles'
-    'hello_world'
+    'project'
 ]
 
 MIDDLEWARE = [
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'hello_django.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['hello_django/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
